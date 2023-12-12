b25e641b74073b7933c4524124cd6e1f1844447f
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-12-11 20:26:01 -0600

    Add loader bar to reflect if the current Navigating state in the UI is loading

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
+import { orange } from "@mui/material/colors";
 
 const RootLayout = () => {
+  const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === "loading" && <LinearProgress />}
         <Outlet />
       </main>
     </>
