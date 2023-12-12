b93d93afc88aa947b90e000520090e0337ea8641
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-12-11 18:44:58 -0600

    Initial commit - Routing and route definitions for Pages: HomePage, EventsPage, EventDetailPage, NewEventPage and EditEventPage

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import RootLayout from "./pages/Layout";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import MainNavigation from "./components/MainNavigation";
+import EventsRootLayout from "./pages/EventsRoot";
+
+// Challenge / Exercise
+
+// 1. Add five new (dummy) page components (content can be simple <h1> elements)
+//    - HomePage
+//    - EventsPage
+//    - EventDetailPage
+//    - NewEventPage
+//    - EditEventPage
+// 2. Add routing & route definitions for these five pages
+//    - / => HomePage
+//    - /events => EventsPage
+//    - /events/<some-id> => EventDetailPage
+//    - /events/new => NewEventPage
+//    - /events/<some-id>/edit => EditEventPage
+// 3. Add a root layout that adds the <MainNavigation> component above all page components
+// 4. Add properly working links to the MainNavigation
+// 5. Ensure that the links in MainNavigation receive an "active" class when active
+// 6. Output a list of dummy events to the EventsPage
+//    Every list item should include a link to the respective EventDetailPage
+// 7. Output the ID of the selected event on the EventDetailPage
+// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+//For this we need another Root Layout EventsRoot.js and move all the affected routes.
+
+function App() {
+  const routesDefinitions = createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="events" element={<EventsRootLayout />}>
+        <Route index={true} element={<EventsPage />} />
+        <Route path=":eventId" element={<EventDetailPage />} />
+        <Route path="new" element={<NewEventPage />} />
+        <Route path=":eventId/edit" element={<EditEventPage />} />
+      </Route>
+    </Route>
+  );
+  const router = createBrowserRouter(routesDefinitions);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
